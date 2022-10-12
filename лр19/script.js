d6bd74349454b3a8a97a83fa31851ec946d6b68b
fckscl <dimapugach31102003@gmail.com>
2022-10-12 16:34:07 +0300

    prompt

--- лр19/script.js
+++ лр19/script.js
@@ -11,9 +11,9 @@ let btn_create = document.getElementById("create")
 let btn_json = document.getElementById("json")
 
 btn_create.onclick = () => {
-    // let new_book = new book(prompt("name"), prompt("author"), prompt("year"), prompt("pages"))
+    let new_book = new book(prompt("name"), prompt("author"), prompt("year"), prompt("pages"))
     // alert(new_book.pages)
-    let new_book = new book('1984', 'oruell', 1964, 300)
+    // let new_book = new book('1984', 'oruell', 1964, 300)
     alert(JSON.stringify(new_book))
     let p = document.createElement("p")
     p.innerText = JSON.stringify(new_book)
